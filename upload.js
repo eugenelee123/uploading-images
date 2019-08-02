209a9070446a0ba810f284daabdf977cb7fb7dbf
Mike <mikecheng@Mikes-MacBook-Pro.local>
2019-08-01 20:59:24 -0700

    SUCCESS(?)

--- upload.js
+++ upload.js
@@ -25,10 +25,10 @@ submitBtn.addEventListener('click', function (event) {
     console.log(err, data);
   });
 
-$.ajax({
-  url: "http://localhost:3000/images",
-  type: "POST",
-  dataType: "json",
-  data: {src: JSON.stringify("http://localhost:9000/images/".concat(imgFile.name)), caption: JSON.stringify(imgName)}
-})
+  $.ajax({
+    url: "http://localhost:3000/images",
+    type: "POST",
+    dataType: "json",
+    data: {src: `http://localhost:9000/images/${imgFile.name}`, caption: imgName.value}
+  })
 });
